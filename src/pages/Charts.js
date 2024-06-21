7a5e95f1be78ea659040e877ed453e79779092f0
Vishal9798 <vishal.p12344@gmail.com>
2024-06-21 23:57:58 +0530

    Changed Data of chart

--- src/pages/Charts.js
+++ src/pages/Charts.js
@@ -3,17 +3,12 @@ import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'r
 
 const data = [
     { name: 'January', caloriesBurned: 4000, workoutTime: 30, weightLoss: 2, workoutsCompleted: 20 },
-    { name: 'February', caloriesBurned: 4200, workoutTime: 32, weightLoss: 1.5, workoutsCompleted: 18 },
-    { name: 'March', caloriesBurned: 4100, workoutTime: 31, weightLoss: 1, workoutsCompleted: 22 },
-    { name: 'April', caloriesBurned: 4300, workoutTime: 33, weightLoss: 1.2, workoutsCompleted: 21 },
+    { name: 'February', caloriesBurned: 3000, workoutTime: 24, weightLoss: 1.5, workoutsCompleted: 18 },
+    { name: 'March', caloriesBurned: 1000, workoutTime: 9, weightLoss: 1, workoutsCompleted: 22 },
+    { name: 'April', caloriesBurned: 6000, workoutTime: 60, weightLoss: 1.2, workoutsCompleted: 21 },
     { name: 'May', caloriesBurned: 4400, workoutTime: 34, weightLoss: 1.8, workoutsCompleted: 19 },
     { name: 'June', caloriesBurned: 4200, workoutTime: 32, weightLoss: 1.3, workoutsCompleted: 20 },
-    { name: 'July', caloriesBurned: 4100, workoutTime: 31, weightLoss: 1.7, workoutsCompleted: 23 },
-    { name: 'August', caloriesBurned: 4000, workoutTime: 30, weightLoss: 1.4, workoutsCompleted: 18 },
-    { name: 'September', caloriesBurned: 3900, workoutTime: 29, weightLoss: 1.5, workoutsCompleted: 21 },
-    { name: 'October', caloriesBurned: 4100, workoutTime: 31, weightLoss: 1.1, workoutsCompleted: 22 },
-    { name: 'November', caloriesBurned: 4300, workoutTime: 33, weightLoss: 1.6, workoutsCompleted: 20 },
-    { name: 'December', caloriesBurned: 4200, workoutTime: 32, weightLoss: 1.9, workoutsCompleted: 19 },
+ 
   ];
   
   
